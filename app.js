a58c3c6b97d84eabc2aa281e2890edd7b1cc0318
Joaquin Kaplan <joaqkaplan@gmail.com>
2022-06-02 11:46:53 +0300

    solve port bug

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
   console.log("server is listening");
 });
 
